77e0c97650bd77cb9f246cd3b124c67b9547de98
nerminaymann <nerminayman1452@gmail.com>
2023-12-29 01:10:32 +0200

    add initil Home,Job_list and Job_Detail Pages

--- job/views.py
+++ job/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
+from .models import Job
 
+# Create your views here.
 
 def Job_List(request):
-    pass
-# Create your views here.
+    jobs= Job.objects.all()
+    context = {'jobs':jobs}
+    return render(request,'job/job_list.html',context)
+
 
 
 def Job_Detail(request,id):
-    pass
+    job = Job.objects.filter(id=id).first()
+    context = {'job': job}
+    return render(request, 'job/job_detail.html', context)
